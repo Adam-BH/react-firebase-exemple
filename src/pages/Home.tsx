ace22ae888c1ffddf47b7ef5f4a3978d5bf4b477
Adam Bhouri <adam.bhouri@gmail.com>
2024-07-26 15:56:41 +0100

    finished auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../config/firebase';
+import { signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+
 export const Home = () => {
-	return <h1>Home page</h1>;
+	const navigate = useNavigate();
+	const [authUser, setAuthUser] = useState({ name: '', email: '' });
+
+	useEffect(() => {
+		const authUserString = localStorage.getItem('auth');
+		if (authUserString !== null) {
+			setAuthUser(JSON.parse(authUserString));
+		} else {
+			navigate('/sign-in');
+		}
+	}, []);
+
+	const logout = () => {
+		signOut(auth).then(() => {
+			navigate('/sign-in');
+			localStorage.clear();
+		});
+	};
+
+	return (
+		<div>
+			<h1>Profile Info</h1>
+			<h2>email: {authUser.email}</h2>
+			<h2>name: {authUser.name}</h2>
+			<button onClick={logout}>Log Out</button>
+		</div>
+	);
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,19 +2,61 @@ import { Link, useNavigate } from 'react-router-dom';
 import './SignUp.css';
 
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../config/firebase';
+import { useEffect } from 'react';
+
+interface userForm {
+	name: string;
+	email: string;
+	pass: string;
+}
 
 export const SignUp = () => {
-	const navigate = useNavigate();
-	const { handleSubmit, register } = useForm();
+	useEffect(() => {
+		const authUserString = localStorage.getItem('auth');
+		if (authUserString !== null) {
+			navigate('/home');
+		}
+	}, []);
 
-	const SubmitData = (data: any) => {
+	const userScheme = yup.object().shape({
+		name: yup.string().required(),
+		email: yup.string().email().required(),
+		pass: yup
+			.string()
+			.min(6, 'password must be at least 6 charachters long')
+			.required(),
+	});
+
+	const navigate = useNavigate();
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<userForm>({
+		resolver: yupResolver(userScheme),
+	});
+
+	const SubmitData = (data: userForm) => {
 		createUserWithEmailAndPassword(auth, data.email, data.pass)
 			.then((userCredentials) => {
-				console.log(userCredentials);
-				navigate('/home');
+				if (auth.currentUser !== null) {
+					updateProfile(auth.currentUser, { displayName: data.name }).then(
+						() => {
+							const authUser = {
+								email: data.email,
+								name: data.name,
+								isAuth: true,
+							};
+							localStorage.setItem('auth', JSON.stringify(authUser));
+							navigate('/home');
+						}
+					);
+				}
 			})
 			.catch((err) => {
 				console.log(err.message);
@@ -25,8 +67,11 @@ export const SignUp = () => {
 		<div>
 			<form onSubmit={handleSubmit(SubmitData)} className="myForm">
 				<input type="text" placeholder="full name" {...register('name')} />
+				<p>{errors.name?.message}</p>
 				<input type="text" placeholder="email" {...register('email')} />
+				<p>{errors.email?.message}</p>
 				<input type="password" placeholder="password" {...register('pass')} />
+				<p>{errors.pass?.message}</p>
 				<input type="submit" />
 			</form>
 			<Link to="/sign-in">Already have an account ?</Link>
